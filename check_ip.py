e2be156d3e7992341c8d3ba27303547a5208c649
HuangKangJing <huangkangjing@gmail.com>
2012-10-26 23:43:28 +0800

    Added sort ip by time support...

--- check_ip.py
+++ check_ip.py
@@ -2,6 +2,7 @@
 import urllib2
 import socket
 import re
+import time
 
 time_out=5
 output_file="./ip"
@@ -9,20 +10,25 @@ base_ip="203.208.47."
 
 def main():
     socket.setdefaulttimeout(time_out)
+    ips={}
     for i in range(1,256):
         ip=base_ip+str(i)
         try:
+            time1=time.time()
             res=urllib2.urlopen("http://%s/"%ip)
+            time2=time.time()
+            t=time2-time1
             if re.search("<title>Google</title>",res.read())!=None:
-                print "IP %s ; Accepted."%ip
-                with open(output_file,"a") as f:
-                    f.write(ip+"\n")
+                print "IP %s ; TIME %f ms ; Accepted."%(ip,t*1000)
+                ips[t]=ip
             else:
                 print "IP %s ; Discarded."%ip
         except Exception as e:
             print "IP %s ; Error Occourred:"%ip
             print e
-
+    ts=ips.keys().sort()
+    with open(output_file,"a") as f:
+        f.write("\n".join(ips[i] for i in ts ))
 
 if __name__ == '__main__':
     main()
