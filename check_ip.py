7230b0a6e24984fec8932ee3a37e297c1501d61a
HuangKangJing <huangkangjing@gmail.com>
2012-10-27 00:19:22 +0800

    Added specified host,request path,pattern support...

--- check_ip.py
+++ check_ip.py
@@ -3,22 +3,33 @@ import urllib2
 import socket
 import re
 import time
+import sys
 
 time_out=5
 output_file="./ip"
-base_ip="203.208.47."
 
-def main():
+def main(specify=False):
     socket.setdefaulttimeout(time_out)
+    base_ip=raw_input("Input base IP:")
+    if specify:
+        host_name=raw_input("Input host name:")
+        request_path=raw_input("Input request path:")
+        pattern=raw_input("Input search pattern:")
+    else:
+        request_path="/"
+        pattern="<title>Google</title>"
     ips={}
     for i in range(1,256):
         ip=base_ip+str(i)
         try:
+            req=urllib2.Request(url="http://%s%s"%(ip,request_path))
+            if specify:
+                req.add_header("Host",host_name)
             time1=time.time()
-            res=urllib2.urlopen("http://%s/"%ip)
+            res=urllib2.urlopen(req)
             time2=time.time()
             t=time2-time1
-            if re.search("<title>Google</title>",res.read())!=None:
+            if re.search(pattern,res.read())!=None:
                 print "IP %s ; TIME %f ms ; Accepted."%(ip,t*1000)
                 ips[t]=ip
             else:
@@ -26,9 +37,11 @@ def main():
         except Exception as e:
             print "IP %s ; Error Occourred:"%ip
             print e
-    ts=ips.keys().sort()
+    ts=ips.keys()
+    ts.sort()
     with open(output_file,"a") as f:
         f.write("\n".join(ips[i] for i in ts ))
 
 if __name__ == '__main__':
-    main()
+    specify="-s" in sys.argv or "--specify" in sys.argv
+    main(specify)
