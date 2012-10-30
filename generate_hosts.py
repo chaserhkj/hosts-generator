9ed5b02b774ff8ee0ba0a597998952419b2985e8
HuangKangJing <huangkangjing@gmail.com>
2012-10-30 11:59:29 +0800

    Added https mode and time_out settings

--- generate_hosts.py
+++ generate_hosts.py
@@ -3,14 +3,18 @@
 import urllib2
 import sys
 import random
+import socket
 
 hosts_list="./host_names"
 ips_list="./ip"
 error_out="./error_out"
 output_file="./hosts"
+
 ua="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4"
+time_out=5
 
 def main(no_check=False):
+    socket.setdefaulttimeout(time_out)
     with open(hosts_list) as f:
         hosts=f.read().split("\n")
         hosts=[i for i in hosts if i]
@@ -33,29 +37,36 @@ def main(no_check=False):
         eo=f.read()
     d={}
     for i in hosts:
-        ipi=iter(ips)
-        while True:
-            try:
-                ip=ipi.next()
-            except StopIteration:
-                print "Host %s ; No IP Matched."%i
-                print "Discard Host %s"%i
-                break
-            req=urllib2.Request(url="http://%s/"%ip,
-                                headers={"Host":i,
-                                         "User-Agent":ua})
-            try:
-                res=urllib2.urlopen(req)
-                if res.read()!=eo:
-                    print "Host %s ; IP %s ; Matched!"%(i,ip)
-                    d[i]=ip
+        for mode in ["http","https"]:
+            print "Host %s ; Start checking in mode %s."%(i,mode)
+            ipi=iter(ips)
+            passed=False
+            while True:
+                try:
+                    ip=ipi.next()
+                except StopIteration:
+                    print "Host %s ; No IP Matched in %s mode."%(i,mode)
                     break
-                else:
-                    print "Host %s ; IP %s ; Not Matched!"%(i,ip)
-            except Exception as e:
-                print "Host %s ; IP %s ; Error Occurred:"%(i,ip)
-                print e
-                continue
+                req=urllib2.Request(url="%s://%s/"%(mode,ip),
+                                    headers={"Host":i,
+                                             "User-Agent":ua})
+                try:
+                    res=urllib2.urlopen(req)
+                    if res.read()!=eo:
+                        print "Host %s ; IP %s ; Matched!"%(i,ip)
+                        passed=True
+                        d[i]=ip
+                        break
+                    else:
+                        print "Host %s ; IP %s ; Not Matched!"%(i,ip)
+                except Exception as e:
+                    print "Host %s ; IP %s ; Error Occurred:"%(i,ip)
+                    print e
+                    continue
+            if passed:
+                break
+        if not passed:
+            print "Discard Host %s."%i
 
     with open(output_file,'w') as f:
         f.write("\n".join("\t".join(i) for i in d.items()))
